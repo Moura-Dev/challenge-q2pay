276601af15c4541f2a5c046c43f7609ca7e5cf06
Luiz Moura <junior.moura19@hotmail.com>
2022-08-27 17:08:29 -0300

    refactor function startdb to postgres

--- db/db.go
+++ db/db.go
@@ -1,20 +1,38 @@
 package db
 
 import (
+	"context"
 	"fmt"
-	"log"
-
 	"github.com/jmoiron/sqlx"
 	_ "github.com/lib/pq"
+	"os"
 )
 
-func StartDB() {
-	db, err := sqlx.Connect("postgres", "user=admin dbname=postgres password=admin sslmode=disable")
-	if err != nil {
-		log.Fatalln(err)
-	}
-	defer db.Close()
+var (
+	err  error
+	Conn *sqlx.DB
+)
 
-	// migrations.RunMigrations(db)
-	fmt.Println("Database connected", db)
+func StartDB() (*sqlx.DB, error) {
+	ctx := context.Background()
+	Conn, err = sqlx.Connect("postgres", os.Getenv("DB_URL"))
+	if err != nil {
+		fmt.Printf("Error connecting to the database: %s", err)
+		return Conn, err
+	}
+
+	Conn.SetMaxIdleConns(100)
+	Conn.SetMaxOpenConns(100)
+
+	ctx, cancel := context.WithCancel(ctx)
+	defer cancel()
+
+	err = Conn.PingContext(ctx)
+	if err != nil {
+		fmt.Printf("Error connecting to the database: %s", err)
+		return Conn, err
+	}
+
+	fmt.Println("Database connected", "Postgres")
+	return Conn, err
 }
