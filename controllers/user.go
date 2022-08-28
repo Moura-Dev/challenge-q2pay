e3d82c060518c1d31fc0406b57a8cabf5aa7732c
Luiz Moura <junior.moura19@hotmail.com>
2022-08-28 01:14:08 -0300

    add validateCNPJ

--- controllers/user.go
+++ controllers/user.go
@@ -3,12 +3,21 @@ package controllers
 import (
 	"challenge-q2pay/models"
 	"challenge-q2pay/repository"
+	"challenge-q2pay/utils"
 	"github.com/gin-gonic/gin"
+	"strconv"
 )
 
 func CreateUser(ctx *gin.Context) {
 	user := models.User{}
 	ctx.ShouldBindJSON(&user)
+	//validate cpf or cnpj
+	if !utils.ValidateCNPJ(user.CpfCnpj) && !utils.ValidateCPF(user.CpfCnpj) {
+		ctx.JSON(500, gin.H{
+			"error": "Invalid cpf or cnpj",
+		})
+		return
+	}
 	err := repository.CreateUser(&user)
 	if err != nil {
 		ctx.JSON(500, gin.H{
@@ -24,7 +33,8 @@ func CreateUser(ctx *gin.Context) {
 // Get User By id
 func GetUser(ctx *gin.Context) {
 	id := ctx.Param("id")
-	data, err := repository.GetUser(id)
+	idInt, err := strconv.Atoi(id)
+	data, err := repository.GetUser(idInt)
 	user := models.User{
 		Id:       data.Id,
 		FullName: data.FullName,
