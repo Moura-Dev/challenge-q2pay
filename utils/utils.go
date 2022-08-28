c4d1e5f6c22ebad131a714549a3f1e9c1354ac9b
Luiz Moura <junior.moura19@hotmail.com>
2022-08-28 13:05:43 -0300

    Func: ValidateCPF ValidateCNPJ ValidateEmail

--- utils/utils.go
+++ utils/utils.go
@@ -1,29 +1,49 @@
-package main
+package utils
 
 import (
 	"encoding/json"
 	"net/http"
+	"regexp"
 	"time"
 )
 
 var myClient = &http.Client{Timeout: 10 * time.Second}
 
-func getJson(url string, target interface{}) error {
+const Url = "https://run.mocky.io/v3/d02168c6-d88d-4ff2-aac6-9e9eb3425e31"
+
+func Authorization(url string, target interface{}) bool {
 	r, err := myClient.Get(url)
 	if err != nil {
-		return err
+		return false
 	}
 	defer r.Body.Close()
 
-	return json.NewDecoder(r.Body).Decode(target)
+	err = json.NewDecoder(r.Body).Decode(&target)
+	if err != nil {
+		return false
+	}
+	authorizeResponse := make(map[string]any)
+	authorizeResponse = target.(map[string]any)
+	if authorizeResponse["authorization"] == false {
+		return false
+	}
+	return true
 }
 
-type T struct {
-	Authorization bool
+func ValidateCPF(cpf string) bool {
+	const pattern = `^\d{3}\.?\d{3}\.?\d{3}-?\d{2}$`
+	matched, _ := regexp.MatchString(pattern, cpf)
+	return matched
 }
 
-func main() {
-	authorization := T{}
-	getJson("https://run.mocky.io/v3/d02168c6-d88d-4ff2-aac6-9e9eb3425e31", &authorization)
-	println(authorization.Authorization)
+func ValidateCNPJ(cnpj string) bool {
+	const pattern = `^^\d{2}\.?\d{3}\.?\d{3}\/?(:?\d{3}[1-9]|\d{2}[1-9]\d|\d[1-9]\d{2}|[1-9]\d{3})-?\d{2}$$`
+	matched, _ := regexp.MatchString(pattern, cnpj)
+	return matched
+}
+
+func ValidateEmail(email string) bool {
+	const pattern = `^[a-zA-Z0-9.!#$%&'*+/=?^_` + "`" + `{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$`
+	matched, _ := regexp.MatchString(pattern, email)
+	return matched
 }
