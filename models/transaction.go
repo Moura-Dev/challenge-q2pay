63fbfc218e6a68d29df920e02208083c055a3596
Luiz Moura <junior.moura19@hotmail.com>
2022-08-30 15:14:34 -0300

    Create Struct for swagger api

--- models/transaction.go
+++ models/transaction.go
@@ -7,5 +7,9 @@ type Transaction struct {
 	Value     decimal.Decimal `json:"value" db:"value"`
 	Payer     int             `json:"payer" db:"payer"`
 	Payee     int             `json:"payee" db:"payee"`
-	CreatedAt string          `json:"created_at" db:"created_at"`
+	CreatedAt string          `json:"-" db:"created_at"`
+}
+
+type Deposit struct {
+	Value decimal.Decimal `json:"value" db:"value"`
 }
