e23a81ea20628a463a8cb48ad1f1536100cd9e60
Luiz Moura <junior.moura19@hotmail.com>
2022-08-30 15:14:13 -0300

    Add Swagger Open api

--- server/routes/routes.go
+++ server/routes/routes.go
@@ -2,11 +2,21 @@ package routes
 
 import (
 	"challenge-q2pay/controllers"
+	_ "challenge-q2pay/docs"
 	"github.com/gin-gonic/gin"
+	swaggerFiles "github.com/swaggo/files"
+	ginSwagger "github.com/swaggo/gin-swagger"
 )
 
+// @title Basic API Transfer Balance
+// @version 1.0
+// @host localhost:5000
+// @BasePath /api
+
 func ConfigRoutes(router *gin.Engine) *gin.Engine {
+
 	main := router.Group("api/")
+
 	{
 		routers := main.Group("/")
 		{
@@ -16,7 +26,8 @@ func ConfigRoutes(router *gin.Engine) *gin.Engine {
 			//Deposit balance user
 			routers.POST("/user/:id/deposit", controllers.DepositBalance)
 			routers.POST("/transfer", controllers.TransferBalance)
-
+			url := ginSwagger.URL("http://localhost:5000/api/docs/doc.json") // The url pointing to API definition
+			routers.GET("/docs/*any", ginSwagger.WrapHandler(swaggerFiles.Handler, url))
 		}
 	}
 	return router
